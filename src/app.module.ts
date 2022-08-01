8538b5b52291234f4b9bab6b702894cb659d6d45
yqiu <qiu5630@163.com>
2022-08-02 00:51:56 +0800

    enhance

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import allModule from './all-module';
 
 @Module({
-  imports: [],
+  imports: [...allModule],
   controllers: [AppController],
   providers: [AppService],
 })
